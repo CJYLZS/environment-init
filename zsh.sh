4349b90f921776aeecdf12c4822ebd38f2065cde
CJYLZS <691086891@qq.com>
2022-07-31 22:02:11 +0800

    update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,10 +1,14 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions
+git clone --depth=1 https://github.com/marlonrichert/zsh-autocomplete.git
 mv zsh-* ~/.oh-my-zsh/custom/plugins/
 rm -rf zsh-*
 
 cp zshrc ~/.zshrc
-cp ylzsalias.sh ~/.ylzsalias.sh
+cp ylzs.zsh-theme ~/.oh-my-zsh/theme/
+cp -f dirhistory.plugin.zsh ~/.oh-my-zsh/plugin/dirhistory/
+source ~/.zshrc
+# cp ylzsalias.sh ~/.ylzsalias.sh
